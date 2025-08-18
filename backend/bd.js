5baf932e5e88be581939838e88947ecf06139b85
Emerson1423 <emersonaldahir2453@gmail.com>
2025-08-17 20:00:20 -0600

    Update bd.js

--- backend/bd.js
+++ backend/bd.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',     // Ej: 'root'
-  password: 'peluchin24', // Reemplaza con tu contraseña
+  password: '', // Reemplaza con tu contraseña
   database: 'huella_de_carbono',
   waitForConnections: true,
   connectionLimit: 10
